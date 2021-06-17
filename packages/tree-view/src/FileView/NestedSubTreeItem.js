6d6438c5c626723cf601c503e613e20458450e6a
robbie roxas <robbieroxas@robbies-MacBook-Pro.local>
2021-06-16 23:01:31 -0700

    keyboard enter/space

--- packages/tree-view/src/FileView/NestedSubTreeItem.js
+++ packages/tree-view/src/FileView/NestedSubTreeItem.js
@@ -17,142 +17,151 @@ import {
 
 import stylesheet from "../presenters/stylesheet";
 
-export function SubTreeItem(props) {
-  const {
-    treeItem,
-    treeItem: {
+export class SubTreeItem extends Component {
+  componentDidUpdate({ keyboardOpenId: previousKeyboardOpenId }) {
+    const {
+      treeItem: { id },
+      keyboardOpenId,
+    } = this.props;
+
+    if (keyboardOpenId === id && keyboardOpenId !== previousKeyboardOpenId) {
+      this.props.treeItem.meta.collapsed = !this.props.treeItem.meta.collapsed;
+      this.props.setIsCollapsed(this.props.treeItem.meta.collapsed);
+      this.props.setKeyboardOpenId("");
+    }
+  }
+  render() {
+    const {
+      treeItem,
+      treeItem: {
+        children,
+        id,
+        meta: { label, icon },
+        payload: {
+          indicator,
+          getActiveTreeItemId,
+          getActiveTreeItemIndex,
+          guidelines,
+          getTreeItemArray,
+          setKeyboardOpenId,
+        },
+      },
+      themeData,
+      onClick,
+    } = this.props;
+
+    const styleTreeItem = {
       children,
       id,
-      meta: { label, icon },
-      payload: {
-        indicator,
-        getActiveTreeItemId,
-        getActiveTreeItemIndex,
-        guidelines,
-        getTreeItemArray,
-        setKeyboardOpenId,
-      },
-    },
-    themeData,
-    onClick,
-  } = props;
-
-  const styleTreeItem = {
-    children,
-    id,
-    label,
-    indicator,
-    themeData,
-    getActiveTreeItemId,
-    getActiveTreeItemIndex,
-    guidelines,
-    selected: getActiveTreeItemId() === id,
-  };
-  const styles = stylesheet(styleTreeItem, themeData);
-  return (
-    <li
-      className={css(styles.higTreeItemSubTreeItem)}
-      id={id}
-      role="treeitem"
-      onClick={(event) => onClick(event, treeItem)}
-      onKeyDown={(event) => {
-        console.log("event.keyCode", event.keyCode);
-        if (event.keyCode === 13 || event.keyCode === 32) {
-          treeItem.meta.collapsed = !treeItem.meta.collapsed;
-        }
-        if (event.keyCode === 38 || event.keyCode === 40) {
-          setKeyboardOpenId(getActiveTreeItemId());
-        }
-      }}
-      tabIndex={-1}
-    >
-      <div className={css(styles.higTreeItemContentWrapper)}>
-        {icon}
-        {label}
-      </div>
-    </li>
-  );
-}
-
-export function NestedSubTreeItem(props) {
-  const {
-    treeItem,
-    treeItem: {
-      children,
-      id,
-      meta: { label, icon, collapsed },
-      payload,
-      payload: {
-        indicator,
-        getActiveTreeItemId,
-        getActiveTreeItemIndex,
-        guidelines,
-        getTreeItemArray,
-        setKeyboardOpenId,
-      },
-    },
-    density,
-    themeData,
-    onClick,
-  } = props;
-
-  if (getActiveTreeItemId() === id) {
-    console.log(
-      "getTreeItemArray",
-      getTreeItemArray().indexOf(getActiveTreeItemIndex())
+      label,
+      indicator,
+      themeData,
+      getActiveTreeItemId,
+      getActiveTreeItemIndex,
+      guidelines,
+      selected: getActiveTreeItemId() === id,
+    };
+    const styles = stylesheet(styleTreeItem, themeData);
+    return (
+      <li
+        className={css(styles.higTreeItemSubTreeItem)}
+        id={id}
+        role="treeitem"
+        onClick={(event) => onClick(event, treeItem)}
+      >
+        <div className={css(styles.higTreeItemContentWrapper)}>
+          {icon}
+          {label}
+        </div>
+      </li>
     );
   }
+}
 
-  const styleTreeItem = {
-    children,
-    id,
-    label,
-    indicator,
-    themeData,
-    getActiveTreeItemId,
-    getActiveTreeItemIndex,
-    guidelines,
-    selected: getActiveTreeItemId() === id,
-  };
+export class NestedSubTreeItem extends Component {
+  componentDidUpdate({ keyboardOpenId: previousKeyboardOpenId }) {
+    const {
+      treeItem: { id },
+      keyboardOpenId,
+    } = this.props;
 
-  const styles = stylesheet(styleTreeItem, themeData);
-  const OperatorMinusIcon =
-    density === "medium-density" ? OperatorMinusSUI : OperatorMinusXsUI;
-  const CaretDownIcon =
-    density === "medium-density" ? CaretDownMUI : CaretDownSUI;
-  const IconIndicator =
-    indicator === "operator" ? OperatorMinusIcon : CaretDownIcon;
-  return (
-    <li
-      aria-expanded={!collapsed}
-      className={css(styles.higTreeItem)}
-      id={id}
-      role="treeitem"
-    >
-      <div className={css(styles.higTreeItemSubTreeViewLabelWrapper)}>
-        <div
-          className={css(styles.higTreeItemSubTreeViewLabelContentWrapper)}
-          onClick={(event) => onClick(event, treeItem)}
-          onKeyDown={(event) => {
-            if (event.keyCode === 13 || event.keyCode === 32) {
-              treeItem.meta.collapsed = !treeItem.meta.collapsed;
-            }
-            if (event.keyCode === 38 || event.keyCode === 40) {
-              setKeyboardOpenId(getActiveTreeItemId());
-            }
-          }}
-          tabIndex={-1}
-        >
-          <IconIndicatorPresenter
-            collapsed={collapsed}
-            density={density}
-            indicator={indicator}
-          />
-          {icon}
-          <span>{label}</span>
+    if (keyboardOpenId === id && keyboardOpenId !== previousKeyboardOpenId) {
+      this.props.treeItem.meta.collapsed = !this.props.treeItem.meta.collapsed;
+      this.props.setIsCollapsed(this.props.treeItem.meta.collapsed);
+      this.props.setKeyboardOpenId("");
+    }
+  }
+  render() {
+    const {
+      treeItem,
+      treeItem: {
+        children,
+        id,
+        meta: { label, icon, collapsed },
+        payload,
+        payload: {
+          indicator,
+          getActiveTreeItemId,
+          getActiveTreeItemIndex,
+          guidelines,
+          getTreeItemArray,
+          setKeyboardOpenId,
+        },
+      },
+      density,
+      themeData,
+      onClick,
+    } = this.props;
+
+    if (getActiveTreeItemId() === id) {
+      console.log(
+        "getTreeItemArray",
+        getTreeItemArray().indexOf(getActiveTreeItemIndex())
+      );
+    }
+
+    const styleTreeItem = {
+      children,
+      id,
+      label,
+      indicator,
+      themeData,
+      getActiveTreeItemId,
+      getActiveTreeItemIndex,
+      guidelines,
+      selected: getActiveTreeItemId() === id,
+    };
+
+    const styles = stylesheet(styleTreeItem, themeData);
+    const OperatorMinusIcon =
+      density === "medium-density" ? OperatorMinusSUI : OperatorMinusXsUI;
+    const CaretDownIcon =
+      density === "medium-density" ? CaretDownMUI : CaretDownSUI;
+    const IconIndicator =
+      indicator === "operator" ? OperatorMinusIcon : CaretDownIcon;
+    return (
+      <li
+        aria-expanded={!collapsed}
+        className={css(styles.higTreeItem)}
+        id={id}
+        role="treeitem"
+      >
+        <div className={css(styles.higTreeItemSubTreeViewLabelWrapper)}>
+          <div
+            className={css(styles.higTreeItemSubTreeViewLabelContentWrapper)}
+            onClick={(event) => onClick(event, treeItem)}
+          >
+            <IconIndicatorPresenter
+              collapsed={collapsed}
+              density={density}
+              indicator={indicator}
+            />
+            {icon}
+            <span>{label}</span>
+          </div>
         </div>
-      </div>
-      <SubTreeViewObjectPresenter {...props} />
-    </li>
-  );
+        <SubTreeViewObjectPresenter {...this.props} />
+      </li>
+    );
+  }
 }
