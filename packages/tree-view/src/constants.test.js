7322947a771d5e855f50817fae4dca7472ff3e3e
robbie roxas <robbieroxas@robbies-MBP.home>
2021-06-27 22:19:29 -0700

    add unit tests

---
+++ packages/tree-view/src/constants.test.js
@@ -0,0 +1,7 @@
+import * as constants from "./constants";
+
+describe("button/constants", () => {
+  it("has an array of available indicators", () => {
+    expect(constants).toHavePropertyOfConstants("AVAILABLE_INDICATORS");
+  });
+});
